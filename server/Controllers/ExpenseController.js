3641161727b2c48ac3353c357fb2a3f3b9bfa597
baoquanong <baoquanong.code@gmail.com>
2022-12-13 22:30:32 +0800

    add expenses

--- server/Controllers/ExpenseController.js
+++ server/Controllers/ExpenseController.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
 const express = require("express");
+const authorization = require("../Middleware/authorization");
 const router = express.Router();
 
 const prisma = new PrismaClient();
@@ -14,6 +15,25 @@ router.get("/seed", async (req, res) => {
     });
 
     res.status(200).json(expenses);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.post("/add", authorization, async (req, res) => {
+  try {
+    const expense = await prisma.expense.create({
+      data: req.body,
+      include: {
+        account: { include: { Expense: true } },
+      },
+    });
+
+    if (!expense) {
+      res.status(400).json({ error: "Unable to create expense" });
+    } else {
+      res.status(200).json(expense);
+    }
   } catch (error) {
     res.status(500).json({ error: error });
   }
