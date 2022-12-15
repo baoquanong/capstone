a769990be31bfc30d77a17b97e42fdfebd9c1893
baoquanong <baoquanong.code@gmail.com>
2022-12-15 23:19:32 +0800

    added victory charts lib

--- client/src/Components/ChartPie.jsx
+++ client/src/Components/ChartPie.jsx
@@ -1,49 +1,53 @@
-import React, { PureComponent } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
-import { VictoryPie, VictoryChart, VictoryAxis, VictoryBar } from 'victory';
+import React, { PureComponent, useEffect, useState } from "react";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+import { VictoryPie, VictoryChart, VictoryAxis, VictoryBar } from "victory";
 
-const data = [
-  {quarter: 1, earnings: 13000},
-  {quarter: 2, earnings: 16500},
-  {quarter: 3, earnings: 14250},
-  {quarter: 4, earnings: 19000}
+const data1 = [
+  { x: "cats", y: 10 },
+  { x: "dogs", y: 50 },
+  { x: "birds", y: 40 },
+];
+const data2 = [
+  { x: "cats", y: 0 },
+  { x: "dogs", y: 0 },
+  { x: "birds", y: 100 },
 ];
 
 function ChartPie() {
+  const [data, setData] = useState(data2);
 
-    return (
+  useEffect(() => {
+    setData(data1);
+  });
+
+  return (
     <div className="chart">
-      <VictoryPie 
-        // domainPadding will add space to each side of VictoryBar to
-        // prevent it from overlapping the axis
+      <VictoryPie
+        animate={{ easing: "exp" }}
+        data={data}
+        innerRadius={100}
         domainPadding={20}
-        >
-        <VictoryAxis
+      >
+        {/* <VictoryAxis
           // tickValues specifies both the number of ticks and where
           // they are placed on the axis
           tickValues={[1, 2, 3, 4]}
           tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
-          />
+        />
         <VictoryAxis
           dependentAxis
           // tickFormat specifies how ticks should be displayed
-          tickFormat={(x) => (`$${x / 1000}k`)}
-          />
-        <VictoryBar
-          data={data}
-          x="quarter"
-          y="earnings"
-          />
+          tickFormat={(x) => `$${x / 1000}k`}
+        /> */}
+        {/* <VictoryBar data={data} x="quarter" y="earnings" /> */}
       </VictoryPie>
     </div>
-    )
-  }
+  );
+}
 
-  export default ChartPie
+export default ChartPie;
 
-
-
-  // const COLORS = [' #EEEEEE', '#CCCCCC', '#666666','#333333'];
+// const COLORS = [' #EEEEEE', '#CCCCCC', '#666666','#333333'];
 
 // const RADIAN = Math.PI / 180;
 // const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
